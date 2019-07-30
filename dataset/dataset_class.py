2f957c77fd9ded3804b44b58a530d386e5db1445
hao66 <hao66@purdue.edu>
2019-07-30 11:30:53 -0400

    add comment

--- dataset/dataset_class.py
+++ dataset/dataset_class.py
@@ -39,7 +39,7 @@ class VidDataSet(Dataset):
         frame_mark = generate_landmarks(frame_mark)
         frame_mark = torch.from_numpy(np.array(frame_mark)).type(dtype = torch.float) #K,2,224,224,3
         frame_mark = frame_mark.transpose(2,4).to(self.device) #K,2,3,224,224
-        frame_mark = frame_mark.transpose(3,4).to(self.device) #K,2,3,224,224
+        frame_mark = frame_mark.transpose(3,4).to(self.device) #K,2,3,224,224 transpose width and height 
         
         g_idx = torch.randint(low = 0, high = self.K, size = (1,1))
         x = frame_mark[g_idx,0].squeeze()
